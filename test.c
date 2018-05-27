c2b54ac31ff0463967062164abc0ac8a2be67fb2
noweprogramtogether <39650954+noweprogramtogether@users.noreply.github.com>
2018-05-27 17:14:17 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main()
+{
+	int ndm; //numero di domande di matematica
+	int ndf; //numero di domande di fisica
+	
+	
+
+	return 0;
+}
+
