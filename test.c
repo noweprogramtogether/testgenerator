6506b4e0a142f7b1a65b95a3207eb453eb033631
noweprogramtogether <39650954+noweprogramtogether@users.noreply.github.com>
2018-05-27 17:24:57 +0200

    Add files via upload

--- test.c
+++ test.c
@@ -8,8 +8,23 @@ int main()
 	int ndm; //numero di domande di matematica
 	int ndf; //numero di domande di fisica
 	
+	void chiedinumerodomande(int*, int*);
+	
+	
 	
 
 	return 0;
 }
 
+
+
+
+void chiedinumerodomande()  //FUNZIONE CHE CHIEDE IL NUMERO DI DOMANDE DA SOMMINISTRARE ALL'UTENTE PER OGNI MATERIA
+{
+	printf("\n Inserisci il numero di domande di matematica:"); //chiedo all'utente quante domande somministrare di matematica
+	scanf("%d", numeromate);                                          //leggo il numero di domande di matematica da somministrare all'utente
+	printf("\n Inserisci il numero di domande di matematica:"); //chiedo all'utente quante domande somministrare di fisica
+	scanf("%d", numerofis);                                          //leggo il numero di domande di fisica da somministrare all'utente
+	
+	return;
+}
